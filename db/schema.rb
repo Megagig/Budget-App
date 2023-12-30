794d78f84889a1f33ee9331857655dfb4a3e095c
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-30 16:41:09 +0100

    Create Group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_30_150201) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_30_153354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_30_150201) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "groups", "users"
 end
