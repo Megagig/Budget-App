5d1a43b3d8c1604f1d95f07783c04807787fbcec
megagig <megagigsoftwaresolutions@gmail.com>
2024-01-02 11:17:31 +0100

    Add validations to the models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :category_details, foreign_key: 'category_id'
+
+  validates :name, presence: true
+  validates :amount, numericality: { only_decimal: true, greater_than_or_equal_to: 0 }
 end
